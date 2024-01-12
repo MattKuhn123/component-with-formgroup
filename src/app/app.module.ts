0f96ba7102022d6e8ff9fd1540c6261333fe7e43
Kuhn, Matthew L <mlkuhn@tva.gov>
2024-01-12 10:14:23 -0500

    move fake error out of service so it is less cluttered

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BioticComponent } from './biotic/biotic.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+export const fake400: HttpErrorResponse = new HttpErrorResponse({ error: "Readable error response from server. Display this on client.", status: 400 });
+export const fake500: HttpErrorResponse = new HttpErrorResponse({ error: "UGLY UNREADABLE DATA! DO NOT DISPLAY", status: 500 });
 
 @NgModule({
   declarations: [
--- src/app/biotic/biotic.service.ts
+++ src/app/biotic/biotic.service.ts
@@ -3,6 +3,7 @@ import { Biotic } from './biotic.form-group';
 import { Observable, delay, map, of } from 'rxjs';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
+import { fake400, fake500 } from '../app.module';
 
 export type PostingStatus = "Not Yet" | "In Progress" | "RecoverableError" | "UnrecoverableError" | "Success";
 
@@ -10,31 +11,26 @@ export type PostingStatus = "Not Yet" | "In Progress" | "RecoverableError" | "Un
   providedIn: 'root'
 })
 export class BioticService {
-
   constructor() { }
 
   protected _postingStatus: WritableSignal<PostingStatus> = signal("Not Yet");
   protected _postingMessage: WritableSignal<string> = signal("");
-
   public postingStatus: Signal<PostingStatus> = this._postingStatus.asReadonly();
   public postingMessage: Signal<string> = this._postingMessage.asReadonly();
 
-  // This is a stubbed method that does not use a REAL HttpClient
   public post(biotic: Biotic): Observable<HttpResponse<unknown>> {
     this._postingStatus.set("In Progress");
     return of(new HttpResponse()).pipe(delay(2000)).pipe(map(x => {
       if (environment.throw400) {
-        const error: HttpErrorResponse = new HttpErrorResponse({ error: "Readable error response from server. Display this on client.", status: 400 });
         this._postingStatus.set("RecoverableError");
-        this._postingMessage.set(error.error);
-        throw error;
+        this._postingMessage.set(fake400.error);
+        throw fake400;
       }
 
       if (environment.throw500) {
-        const error: HttpErrorResponse = new HttpErrorResponse({ error: "UGLY UNREADABLE DATA! DO NOT DISPLAY", status: 500 });
         this._postingStatus.set("UnrecoverableError");
         this._postingMessage.set("An error has occured");
-        throw error;
+        throw fake500;
       }
 
       this._postingStatus.set("Success");
