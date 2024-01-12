cd23bcf4b2a714cabac2b0ab8794ac298d8e2544
Kuhn, Matthew L <mlkuhn@tva.gov>
2024-01-12 11:01:02 -0500

    effects

--- src/app/biotic/biotic.component.ts
+++ src/app/biotic/biotic.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, effect } from '@angular/core';
 import { BioticService } from './biotic.service';
 import { Biotic, BioticFormGroup } from './biotic.form-group';
 
@@ -7,14 +7,32 @@ import { Biotic, BioticFormGroup } from './biotic.form-group';
   templateUrl: './biotic.component.html',
   styleUrls: ['./biotic.component.css']
 })
-export class BioticComponent {
+export class BioticComponent implements OnInit {
   protected bioticForm: BioticFormGroup = new BioticFormGroup();
-  constructor(protected bioticService: BioticService) { }
+
+  protected disableSubmit: boolean = true;
+  protected requestInProgess: boolean = false;
+  protected requestComplete: boolean = false;
+  protected message: string = "";
+
+  constructor(private bioticService: BioticService) {
+    effect(() => {
+      this.updateDisableSubmit();
+      this.requestInProgess = this.bioticService.postingStatus() === "In Progress";
+      this.requestComplete = ['RecoverableError', 'UnrecoverableError', 'Success'].includes(this.bioticService.postingStatus());
+      this.message = this.bioticService.postingMessage();
+    });
+  }
+
+  ngOnInit(): void {
+    this.bioticForm.valueChanges.subscribe(_ => this.updateDisableSubmit());
+  }
+
+  private updateDisableSubmit(): void {
+    this.disableSubmit = this.bioticForm.invalid || this.bioticService.postingStatus() === 'In Progress';
+  }
 
   protected onClickSubmit(): void {
-    const bioticFormValue: Biotic = this.bioticForm.getRawValue();
-    this.bioticService.post(bioticFormValue).subscribe(_ => {
-      this.bioticForm.markAsPristine();
-    })
+    this.bioticService.post(this.bioticForm.getRawValue()).subscribe(_ => this.bioticForm.markAsPristine());
   }
 }
